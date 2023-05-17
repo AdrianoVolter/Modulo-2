3a0f108fd82bad84f021c2d4b8a4f6b7f9287680
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-05-17 14:07:41 -0300

    Ex-4: função ler arquico json

--- M2-s03/index.js
+++ M2-s03/index.js
@@ -1,6 +1,18 @@
 const http = require('http');
 const fs = require('fs');
 
+
+// Crie uma função para ler os dados do arquivo JSON e retornar todos em uma lista como resultado.
+
+function lerDados() {
+      fs.readFile('dados.json', (err, data) => {
+         if (err) throw err;
+         let dados = JSON.parse(data);
+         console.log(dados);
+      });
+   }
+
+
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end("<>Hello World!")
