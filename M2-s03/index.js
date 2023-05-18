9bcf4dfa82692f4b35f2a0ba1a24ca85ff4d1931
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-05-18 15:46:50 -0300

    Ex-6: Criando a função de cadastro de produto

--- M2-s03/index.js
+++ M2-s03/index.js
@@ -12,27 +12,53 @@ function lerDados() {
       });
    }
 
+function CriarProduto(novoProduto){
+   try {
+         const dados = JSON.parse(fs.readFileSync("dados.json", "utf-8"))
+         dados.produtos.push(JSON.parse(novoProduto))
+         fs.writeFileSync("dados.json", JSON.stringify(dados))
+         return "Produto cadastrado com sucesso!"
+         } catch {
+         return "Erro ao executar"
+         }
+      }
+
+// Crie uma função para criar um novo produto no arquivo JSON. O novo produto deve ser passado como parâmetro para a função.
+
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end("<>Hello World!")
-
-
-   switch (req.method) {
-
-      case 'GET':
-         response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' }) // aqui vai a lógica do GET - exercício 6
-         response.write(lerDados()) 
-         response.end()    
-
-      break
-
-      case 'POST':
-
-         // aqui vai a lógica do POST - exercício 7
-      break
-
+   if (req.url == '/') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end("Você está na página inicial")
    }
+   else if (req.url == '/produto') {
+
+      switch (req.method) {
+
+         case 'GET':
+            response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' }) // aqui vai a lógica do GET - exercício 6
+            response.write(lerDados()) 
+            response.end()    
+
+         break
+
+         case 'POST':
+
+            let data = ''
+            req.on('data', (chunk) => {
+               data += chunk
+            })
+            req.on('end', () => {
+               response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' }) // aqui vai a lógica do POST - exercício 7
+               response.end(CriarProduto(data))
+            })
+
+
+         break
+
+      }
+   }
+   
 
 })
 server.listen(3050)
