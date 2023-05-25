c9d727328b94abfdf909baa5d57644423df5badf
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-05-25 14:51:06 -0300

    feat: aula 3 material

--- semana-4/aula-3/src/utils.js
+++ semana-4/aula-3/src/utils.js
@@ -1,16 +1,33 @@
-//const fs = require('fs');
-
+const sistemasDeArquivos = require('fs')
 
 function validaEmail(email) {
-    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  
-    return regex.test(email);
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  return regex.test(email);
+}
+
+function criarOuAtualizar(nomeDoArquivo, dados) {
+  try {
+    sistemasDeArquivos.writeFileSync('src/database/' + nomeDoArquivo, 
+    JSON.stringify(dados))
+  } catch (error) {
+    throw error
   }
-  
+}
 
-
-    
-
-  module.exports = {
-    validaEmail
+function pegarDados(nomeDoArquivo) {
+  try {
+    const resultadoDaBusca = JSON.parse(
+      sistemasDeArquivos.readFileSync('src/database/' + nomeDoArquivo, 'utf8')
+      )
+    return resultadoDaBusca
+  } catch (error) {
+    throw error
   }
+}
+
+module.exports = {
+  validaEmail,
+  criarOuAtualizar,
+  pegarDados
+}
