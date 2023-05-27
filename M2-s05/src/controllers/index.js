3556f68d433972caa181c8137858ca8886487c2c
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-05-26 23:39:24 -0300

    feat: endpoint patch , reagras 1 , 2 e 3

--- M2-s05/src/controllers/index.js
+++ M2-s05/src/controllers/index.js
@@ -1,16 +1,39 @@
 module.exports = {
 
     async recebaLista (req, res) {
-        let lista = ['Pedro', 'José', 'Aderbal', 'Danilo', 'Luisa', 'Vitoria']
-        if(!lista){
-             res.status(404).json({error: 'Lista não encontrada'}
-            )
-        }
-        res.status(200).json(lista)
-        console.log("Lista recebida com sucesso")
-        
 
-       
-    },
+        let lista = ['Pedro', 'José', 'Aderbal', 'Danilo', 'Luisa', 'Vitoria']
+        const { nomes } = req.body
+
+        //// Regra 1: Trocar a posição do índice 0 por qualquer outro item da lista
+
+        if(nomes.includes(lista[0] && nomes.length > 1)){
+            const index = nomes.indexOf(lista[0])
+            const indexAleatorio = Math.floor(Math.random() * (nomes.length - 1)) + 1;
+            [nomes[0], nomes[indexAleatorio]] = [nomes[indexAleatorio], nomes[0]]
+            //console.log(nomes)
+       } 
+       // Regra 2: Colocar o 'Danilo' no topo da lista, trocando de lugar com o 'Pedro'
+
+        if(nomes.includes(lista[0] && nomes.length > 1)){
+            const daniloIndex = nomes.indexOf('Danilo')
+            const pedroIndex = nomes.indexOf('Pedro')
+            [nomes[daniloIndex], nomes[0]] = [nomes[0], nomes[daniloIndex]]
+            [nomes[pedroIndex], nomes[1]] = [nomes[1], nomes[pedroIndex]]
+            //console.log(nomes)
+            }
+        // Regra 3: Verificar se algum nome não pertence à lista
+
+        const nomesInvalidos = nomes.filter(nome => !lista.includes(nome))
+        if(nomesInvalidos.length > 0){
+            const mensagem = `Os nomes ${nomesInvalidos} não pertencem à lista`
+            return res.status(400).json(mensagem)
+        }
+
+        lista = nomes
+        return res.status(200).json(lista)
+        
+    }
+    
 }
     
--- M2-s05/src/index.js
+++ M2-s05/src/index.js
@@ -4,7 +4,7 @@ const router = require('./routes/rota')
 
 const PORT = 3000
 
-//  app.use(express.json())
+app.use(express.json())
 // app.use(rotas)
 
 app.get('/', (req, res) => {
