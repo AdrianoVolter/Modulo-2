f57cc156440827594bb7b8c3a160e18b03a36d9d
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-05-29 19:03:42 -0300

    feat: Exercicio 5

--- M2-s05/src/controllers/retornaDados.js
+++ M2-s05/src/controllers/retornaDados.js
@@ -1,31 +1,27 @@
 module.exports = {
 
-// Utilizando o arquivo user.json fornecido no repositório de exemplo, crie um endpoint do tipo GET que deverá retornar os dados de acordo com o filtro aplicado na Query da requisição.
-
-// Utilize os seguintes critérios no filtro:
-
-// ageMin
-// ageMax
-// State
-// Job
 
     async retornaDados(req, res) { //recebe o body da requisição
+
         const fs = require('fs');
+        const { ageMin, ageMax, state, job } = req.query; //recebe o body da requisição
+        const data = fs.readFileSync('src/database/user (1).json', 'utf8'); //lê o arquivo json
+        const users = JSON.parse(data); //converte o arquivo json em objeto
 
-        const { ageMin, ageMax, state, job } = req.query;
+        const usersFiltered = users.filter(user => { //filtra os dados do arquivo json
 
-        const data = fs.readFileSync('src/database/user (1).json', 'utf8');
-        const users = JSON.parse(data);
+            if (ageMin && user.age < ageMin) return false; //se a idade for menor que a idade mínima, retorna false
 
-        const usersFiltered = users.filter(user => {
-            if (ageMin && user.age < ageMin) return false;
-            if (ageMax && user.age > ageMax) return false;
-            if (state && user.state !== state) return false;
-            if (job && user.job !== job) return false;
-            return true;
+            if (ageMax && user.age > ageMax) return false; //se a idade for maior que a idade máxima, retorna false
+
+            if (state && user.state !== state) return false; //se o estado for diferente do estado informado, retorna false
+
+            if (job && user.job !== job) return false; //se o trabalho for diferente do trabalho informado, retorna false
+
+            return true; //se não for nenhum dos casos acima, retorna true
         }   
         );
 
-        res.send(usersFiltered);
+        res.send(usersFiltered); //retorna os dados filtrados
     }
-}
+};
