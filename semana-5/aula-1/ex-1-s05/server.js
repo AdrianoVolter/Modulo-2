ed006c23496d908859d60c1f316f809140a7e21c
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-05-30 19:02:38 -0300

    feat: instalação do cors

--- semana-5/aula-1/ex-1-s05/server.js
+++ semana-5/aula-1/ex-1-s05/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const rotasDaEmpresa = require('./router/empresaRotas');
 const PORT = 3000;
 
 app.use(express.json());
 app.use(rotasDaEmpresa);
+app.use(cors());
 
 app.listen(PORT, () => {
 console.log(`Server is running on port http://localhost:${PORT}`);
