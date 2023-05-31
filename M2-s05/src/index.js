71d386e9ab7a0f8ce930df7c39ab6c6fdc46122e
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-05-31 16:20:10 -0300

    feat: endpoint do tipo PUT para efetuar a alteração de um arquivo JSON

--- M2-s05/src/index.js
+++ M2-s05/src/index.js
@@ -4,8 +4,6 @@ const router = require('./routes/rota')
 const PORT = 3000
 
 app.use(express.json())
-// app.use(rotas)
-
 app.use(router)
 
 app.listen(PORT, () => console.log(`Servidor ligado na porta http://localhost:${PORT}`))
--- M2-s05/src/routes/rota.js
+++ M2-s05/src/routes/rota.js
@@ -3,11 +3,13 @@ const router = Router();
 const {recebaLista, listarData} = require('../controllers/index');
 const {enviarJson} = require('../controllers/enviarJson');
 const {retornaDados} = require('../controllers/retornaDados');
+const {alterarJson} = require('../controllers/alterarJson');
 
 
 router.patch('/lista', recebaLista);
 router.get('/datas/:mes', listarData);
 router.post('/enviarJson', enviarJson);
 router.get('/retornaDados', retornaDados);
+router.put('/alterarJson/:id', alterarJson);
 
 module.exports = router;
