a8840810ea5d5eab8249c15b0f6052dd3ecb9685
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-06-07 12:16:10 -0300

    feat: comandos sql aula 2 , ajustes

--- semana-6/aula-2/aula-2.sql
+++ semana-6/aula-2/aula-2.sql
@@ -5,7 +5,7 @@ CREATE TABLE Autor (
     data_nascimento DATE
 );
 
-select * from autor;
+SELECT * FROM autor;
 
 CREATE TABLE editora (
     id SERIAL PRIMARY KEY,
@@ -14,9 +14,7 @@ CREATE TABLE editora (
     fundacao DATE
 );
 
-select * from editora;
-
-
+SELECT * FROM editora;
 
 CREATE TABLE livro (
     id SERIAL PRIMARY KEY,
@@ -24,38 +22,33 @@ CREATE TABLE livro (
     ano_publicacao DATE,
     autor_id INTEGER,
     editora_id INTEGER,
-    foreign key (autor_id) references autor(id) ,
-    foreign key (editora_id) references editora(id) 
+    FOREIGN KEY (autor_id) REFERENCES autor(id),
+    FOREIGN KEY (editora_id) REFERENCES editora(id)
 );
 
-select * from livro;
+SELECT * FROM livro;
 
-insert into editora (nome, pais, fundacao) values 
-	('Brasil', 'Brasil' , '2020-02-23'),
-	('Brasil', 'Brasil' , '2020-02-23'),
-	('Brasil', 'Brasil' , '2020-02-23');
-	
+INSERT INTO editora (nome, pais, fundacao) VALUES 
+    ('Brasil', 'Brasil', '2020-02-23'),
+    ('Brasil', 'Brasil', '2020-02-23'),
+    ('Brasil', 'Brasil', '2020-02-23');
 
-insert into autor (nome, nacionalidade, data_nascimento) values 
-	('Adriano', 'Brasil' , '1984-02-23'),
-	('Adr', 'Brasil' , '1999-02-21'),
-	('Adriano', 'Brasil' , '1984-02-23');
-	
-select * from autor;
-	
-	
-INSERT INTO livro (titulo, ano_publicacao, autor_id, editora_id) values 
-	('Livro 1', '1987-21-31' , 
-	 COALESCE((SELECT id FROM autor), 0),
-	 COALESCE((SELECT id FROM editora), 0)
-	),
-	('Livro 2', '1999-01-31' , 
-	 COALESCE((SELECT id FROM autor), 0),
-	 COALESCE((SELECT id FROM editora), 0)
-	),
-	('Livro 3', '1977-21-22' , 
-	 COALESCE((SELECT id FROM autor), 0),
-	 COALESCE((SELECT id FROM editora), 0)
-	);
+INSERT INTO autor (nome, nacionalidade, data_nascimento) VALUES 
+    ('Adriano', 'Brasil', '1984-02-23'),
+    ('Adr', 'Brasil', '1999-02-21'),
+    ('Adriano', 'Brasil', '1984-02-23');
 
+SELECT * FROM autor;
 
+INSERT INTO livro (titulo, ano_publicacao, autor_id, editora_id) VALUES 
+    ('Livro 1', '1987-01-31',
+    (SELECT id FROM autor WHERE nome = 'Adriano' LIMIT 1),
+    (SELECT id FROM editora WHERE nome = 'Brasil' LIMIT 1)),
+    ('Livro 2', '1987-02-28',
+    (SELECT id FROM autor WHERE nome = 'Adr' LIMIT 1),
+    (SELECT id FROM editora WHERE nome = 'Brasil' LIMIT 1)),
+    ('Livro 3', '1987-03-15',
+    (SELECT id FROM autor WHERE nome = 'Adriano' LIMIT 1),
+    (SELECT id FROM editora WHERE nome = 'Brasil' LIMIT 1));
+
+SELECT * FROM livro;
