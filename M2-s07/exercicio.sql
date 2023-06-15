0d9de78aa3425909716676b2adcc1ae1eca3ea2a
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-06-15 15:12:26 -0300

    feat: exercicio 2 semana 7

---
+++ M2-s07/exercicio.sql
@@ -0,0 +1,44 @@
+-- tabela: usuario
+create table usuario (
+    id serial primary key,
+    nome varchar(100) ,
+    login varchar(100),
+    senha varchar(100),
+    email varchar(100),
+    dt_nascimento date ,
+    cpf varchar(100),
+    rg varchar(100),
+    check  
+);
+
+select * from usuario;
+
+-- tabela: jogo
+create table jogo (
+    id serial primary key,
+    nome varchar(100),
+    data_lancamento date,
+    plataforma varchar(100),
+    genero varchar(100),
+    foto varchar(100),
+    video varchar(100)
+);
+
+select * from jogo;
+
+-- tabela: plataforma
+create table plataforma (
+    id serial primary key,
+    nome varchar(100)
+);
+
+select * from plataforma;
+
+-- tabela: genero
+create table genero (
+    id serial primary key,
+    nome varchar(100)
+);
+
+select * from genero;
+
