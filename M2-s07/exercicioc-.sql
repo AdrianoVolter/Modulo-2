5903c609e2917458e2dae6ca475460179fc7d383
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-06-19 17:39:07 -0300

    feat: exercicios da semana 7

--- M2-s07/exercicioc-.sql
+++ M2-s07/exercicioc-.sql
@@ -27,6 +27,14 @@ values ('Derpson da Silva'),
 -- 2 vendsa por usuario exemplo
 
 insert into venda (id_usuario, id_produto, valor_produto, quantidade_produto, dt_compra)
+values (1, 1, 10.00, 1, '2020-11-01'),
+       (1, 2, 20.00, 1, '2020-11-01'),
+       (2, 1, 10.00, 1, '2020-11-01'),
+       (2, 2, 20.00, 1, '2020-11-01'),
+       (3, 1, 10.00, 1, '2020-11-01'),
+       (3, 2, 20.00, 1, '2020-11-01'),
+       (4, 1, 10.00, 1, '2020-11-01'),
+       (4, 2, 20.00, 1, '2020-11-01');
 
 --() Crie um script que ordene os usuários que mais gastaram no mês da Black Friday, independente do ano. Esse script deve conter o nome do usuário e o valor total gasto no mês da black friday
 
@@ -36,3 +44,44 @@ inner join venda v on v.id_usuario = u.id_usuario
 where extract(month from v.dt_compra) = 11
 group by u.nome_usuario
 order by valor_total desc;
+
+select u.nome_usuario, sum(v.valor_produto) as valor_total
+from usuario_cliente u
+inner join venda v on v.id_usuario = u.id_usuario_cliente
+where extract(month from v.dt_compra) = 11
+group by u.nome_usuario
+order by valor_total desc;     
+      
+ -- alguns testes para o exercicio     
+insert into venda (id_usuario, id_produto, valor_produto, quantidade_produto, dt_compra)
+values (1, 1, 10.00, 1, '2020-11-01'),
+       (1, 2, 20.00, 1, '2020-11-01'),
+       (2, 1, 10.00, 1, '2020-11-01'),
+       (2, 2, 20.00, 1, '2020-11-01'),
+       (3, 1, 10.00, 1, '2020-11-01'),
+       (3, 2, 20.00, 1, '2020-11-01'),
+       (4, 1, 10.00, 1, '2020-11-01'),
+       (4, 2, 20.00, 1, '2020-11-01');  
+
+
+insert into venda (id_usuario, id_produto, valor_produto, quantidade_produto, dt_compra)
+values (4, 4, 100.00, 1, '2020-11-01'),
+       (2, 6, 200.00, 1, '2020-11-01'),
+       (3, 4, 100.00, 1, '2020-11-01');
+       
+      
+select * from venda;
+
+--update venda set valor_produto = 500 where id_produto = 1;
+update venda 
+set valor_produto = 500
+where id_produto = 1;
+      
+update venda 
+set valor_produto = 250
+where id_produto = 4;
+      
+select Valor_produto 
+from venda v
+where id_produto = 4;    
+      
