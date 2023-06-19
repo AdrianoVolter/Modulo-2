d712ccb13c7ac4488012da6c2b158574843efd88
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-06-19 13:05:39 -0300

    feat: exercicio 6 semana 7

--- M2-s07/exercicio-5.sql
+++ M2-s07/exercicio-5.sql
@@ -27,3 +27,16 @@ from pais p
 inner join estado e on e.id_pais = p.id_pais
 inner join cidade c on c.id_estado = e.id_estado
 where c.capital = 'True' and c.qtd_populacao > 500000 and p.continente = 'América do Norte' or p.continente = 'América Central' or p.continente = 'América do Sul' and e.nome_estado like 'São%' or e.nome_estado like 'Santo%' or e.nome_estado like 'San%' or e.nome_estado like 'Saint%'
+
+-- Considerando o script do exercício anterior [M2S07] Ex 5, crie um script que busque, ao invés dos 10 países que tenham mais habitantes, os países que contenham população maior que a população total da Espanha (calculada como a soma da população de todas as cidades).
+
+-- Dica: é necessário utilizar o Having
+
+select p.nome_pais, sum(c.qtd_populacao) as populacao
+from pais p
+inner join estado e on e.id_pais = p.id_pais
+inner join cidade c on c.id_estado = e.id_estado
+group by p.nome_pais
+having sum(c.qtd_populacao) > (select sum(qtd_populacao) from cidade where id_estado = 2)
+
+
