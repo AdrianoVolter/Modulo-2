8c26746cc6ad1d09a79149da6a953fd98d9eae17
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-06-19 10:03:39 -0300

    feat: exercicio 3 semana 7

--- M2-s07/exercicio.sql
+++ M2-s07/exercicio.sql
@@ -30,11 +30,20 @@ select * from jogo;
 create table plataforma (
     id serial primary key,
     nome varchar(100),
-    fk_jogo integer references jogo(id),
-    fk_usuario integer references usuario(id)
+    fk_jogo integer  ,
+    fk_usuario integer ,
+    foreign key (fk_jogo) references jogo(id),
+    foreign key (fk_usuario) references usuario(id)
 );
 
 select * from plataforma;
 
+
+insert into jogo (nome, data_lancamento, plataforma, genero, foto, video)
+values ('The Sims 4 Base', '2014-09-02', 'Playstation 4, Playstation 5, Desktop', 'Simulação da Vida Real', 'foto', 'video');
+
+
+insert into usuario (nome, login, senha, email, dt_nascimento, cpf, rg)
+values ('Derpson da Silva', 'derpinho', 'derpinho91', 'derpinho91@hotmail.com', '1991-01-01', '123.123.123-12', '4.123.123');
 
 
