602a21dd58cd57a4f80962c5ac89837859361815
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-06-21 21:44:23 -0300

    feat: aula 3 semana 8 , exercico do trello

--- M2-s08/src/database/migrations/20230621231722-create-table-category.js
+++ M2-s08/src/database/migrations/20230621231722-create-table-category.js
@@ -3,12 +3,26 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+          type: Sequelize.STRING,
+          allowNull: false
+      },
+      created_at: {
+          type: Sequelize.DATE,
+          allowNull: false
+      },
+      updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
