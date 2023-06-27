b133883c89dff736f534fc1d9eb4eae9b3f26d10
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-06-27 12:05:15 -0300

    feat: exercicio 5 s08

--- M2-s08/src/models/trainee.js
+++ M2-s08/src/models/trainee.js
@@ -1,27 +1,51 @@
 const connection = require('../database/connection');
-const {STRING,DATE} = require('sequelize');
+const { DataTypes } = require('sequelize');
 
 
 const Trainees = connection.define('trainees', {
-    name: STRING,
-    email: STRING,
-    rg: STRING,
-    cpf: STRING,
-    primary_phone_contact: STRING,
-    secondary_phone_contact: STRING,    
-    date_birth: DATE,
-    father_name: STRING,
-    mother_name: STRING,
-    have_special_needs: STRING,
-    created_at: DATE,
-    updated_at: DATE
-// }, {
-//     underscored: true,
-//     timestamps: true,
-//     createdAt: 'created_at',
-//     updatedAt: 'updated_at',
-//     tableName: 'trainees'
-}
-);
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      rg: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      cpf: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      primary_phone_contact: {
+        type: DataTypes.STRING,
+      },
+      secondary_phone_contact: {
+        type: DataTypes.STRING,
+      },
+      date_birth: {
+        type: DataTypes.DATE,
+      },
+      father_name: {
+        type: DataTypes.STRING,
+      },
+      mother_name: {
+        type: DataTypes.STRING,
+      },
+      have_special_needs: {
+        type: DataTypes.BOOLEAN,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE, 
+        allowNull: false,
+      },
+    
+});
 
 module.exports = Trainees;
